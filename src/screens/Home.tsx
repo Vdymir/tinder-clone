3dc7f5b8ad3ffd3e0fe79f7c931c17deb55d7eb3
vladimir <vladistar200302@gmail.com>
2022-06-19 23:44:33 -0500

    feat: add animations

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import TinderCard from '../components/TinderCard';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  useAnimatedGestureHandler,
+} from 'react-native-reanimated';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
 export default function Home() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        translateY: translateY.value,
+      },
+    ],
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onActive: event => {
+      translateX.value = event.translationX;
+      translateY.value = event.translationY;
+    },
+  });
+
   return (
     <View style={styles.main}>
-      <TinderCard
-        name="Zuck"
-        bio="No need to send me your nudes, I already saw them"
-        img="https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg"
-      />
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.animatedCard, cardStyle]}>
+          <TinderCard
+            name="Zuck"
+            bio="No need to send me your nudes, I already saw them"
+            img="https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg"
+          />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 }
@@ -21,4 +53,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#FFF',
   },
+  animatedCard: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  change: {},
 });
